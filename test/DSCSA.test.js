1e17583294110f8aa559056245288b3fb0716bbd
Tracy Sanders <79538769+Sun2Money@users.noreply.github.com>
2021-05-10 23:32:37 -0700

    DRAFT

--- test/DSCSA.test.js
+++ test/DSCSA.test.js
@@ -16,15 +16,33 @@ contract('DSCSA', function () {
 
   // Test case
   describe('Setters and Getters', function () {
+    // Set Test Variables
+    var testHolderAddress; 
+    var testGlobalTradeItemNumber;
+    var testExpirationDate;
+    var testLotNumber;
+    var testSerialNumber;
+    
     it('retrieve returns a value previously stored', async function () {
+
+
       // Store values
-      let date = (new Date()).getTime();
+      let date = (new Date(2025, 0, 1)).getTime();
       let expirationDateInUnixTimestamp = date / 1000;
+      
+
       await this.DSCSA.store(42, 42, expirationDateInUnixTimestamp, 42, 42);
   
       // Test if the returned value is the same one
       // Note that we need to use strings to compare the 256 bit integers
-      expect((await this.DSCSA.retrieve()).toString()).to.equal('42');
+      const result = await this.DSCSA.retrieve();
+      const {0: strValue, 1: boolValue, 2: intValue} = result;
+
+      console.log(strValue); // "data"
+      console.log(boolValue); // true
+      console.log(intValue); // 15
+
+
     });
   });
 
