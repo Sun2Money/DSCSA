57f4bc55e99672f6956468b32fb547c7083ba526
Tracy Sanders <79538769+Sun2Money@users.noreply.github.com>
2021-05-18 11:00:52 -0700

    DRAFT

--- test/DSCSA.test.js
+++ test/DSCSA.test.js
@@ -1,4 +1,6 @@
 // Load dependencies
+const { accounts } = require('@openzeppelin/test-environment');
+const [ manufacturer, repackagers, distributor, wholesaler, dispensers ] = accounts;
 const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 const { ZERO_ADDRESS } = constants;
 const { expect, assert } = require('chai');
@@ -6,29 +8,36 @@ const { expect, assert } = require('chai');
 // Load compiled artifacts
 const DSCSA = artifacts.require('DSCSA');
 
-// Start test block: lot = 'xyz123' (maximum is 20 characters)
-contract('dscsa set/get tests', function () {
-  // Set Test Variables
-  var testHolderAddress; 
-  var testGlobalTradeItemNumber = 855245005019;
-  var testExpirationDate;
-  var testLot = 'xyz123';
-  var testSerialNumber = 477018182632;
-  
-  let date = (new Date(2025, 0, 1)).getTime();
-  testExpirationDate = date / 1000;
+// Define Global Test Variables
+var testHolderAddress; 
+var testGlobalTradeItemNumber = 855245005019;
+var testExpirationDate;
+var testLot = 'xyz123';
+var testSerialNumber = 477018182632;
 
+// Set global variables
+let date = (new Date(2025, 0, 1)).getTime();
+testExpirationDate = date / 1000;
+
+//-----------------------------------------------------------------------------
+// set/get tests : Assign contract property and retrieve to test
+// store/retrieve: Assign all contract properties and read to test
+//-----------------------------------------------------------------------------
+contract('dscsa set/get tests', function () {
+
+  // Deploy a new DSCSA contract for each test
   beforeEach(async function () {
-    // Deploy a new DSCSA contract for each test
     this.DSCSA = await DSCSA.new();
   });
 
   it('set/get holderAddress', async function () {
     testHolderAddress = await this.DSCSA.owner();
-    await this.DSCSA.setHolderAddress(testHolderAddress);
+
     assert( testHolderAddress > 0, 'owner is not empty');
 
+    await this.DSCSA.setHolderAddress(testHolderAddress);
     let rtnHolderAddress = await this.DSCSA.getHolderAddress();
+
     assert( testHolderAddress == rtnHolderAddress, 'holder != set');    
   });
 
@@ -71,5 +80,45 @@ contract('dscsa set/get tests', function () {
     assert( testLot == rtnLot, 'lot != set');
     assert( testSerialNumber == rtnSerialNumber, 'serialNumber != set');    
   }); 
+});
+
+//-----------------------------------------------------------------------------
+// Simple scenario where the MAH logs all data for DSCSA from internal systems
+//-----------------------------------------------------------------------------
+contract('dscsa MAH logs all supply chain activity', function () {
+
+  beforeEach(async function () {
+    this.DSCSA = await DSCSA.new();
+  });
+
+  it('Owner (MAH) documents supply chain activity', async function () {
+    // owner == manufacturer
+    testHolderAddress = await this.DSCSA.owner();
+    await this.DSCSA.store(testHolderAddress, testGlobalTradeItemNumber, testExpirationDate, testLot, testSerialNumber);  
+
+    // set to repackagers
+    await this.DSCSA.setHolderAddress(repackagers);
+    let rtnAddress = await this.DSCSA.getHolderAddress();
+
+    assert( repackagers == rtnAddress, 'holder != repackagers'); 
+
+    // set to distributor
+    await this.DSCSA.setHolderAddress(distributor);
+    rtnAddress = await this.DSCSA.getHolderAddress();
+
+    assert( distributor == rtnAddress, 'holder != distributor'); 
+
+    // set to wholesaler
+    await this.DSCSA.setHolderAddress(wholesaler);
+    rtnAddress = await this.DSCSA.getHolderAddress();
+
+    assert( wholesaler == rtnAddress, 'holder != wholesaler');    
+
+    // set to dispensers
+    await this.DSCSA.setHolderAddress(dispensers);
+    rtnAddress = await this.DSCSA.getHolderAddress();
+
+    assert( dispensers == rtnAddress, 'holder != dispensers');
+  });  
 
 });
